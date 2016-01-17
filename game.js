2586c62c538971174dfdb3bf4db236c3a0d01e58
Daniel Wade <dcwade87@gmail.com>
2016-01-17 11:27:52 -0800

    Added function and brushed up while loops

--- game.js
+++ game.js
@@ -1,99 +1,118 @@
 
-                    var myAge = [28, "twenty eight", "twenty-eight", "twentyeight"];
-                    var correctGuess = 0
-                    var myNumber = 8
-                    var myNumberCount = 0
+  var correctGuess = 0
 
-                    alert("Welcome to this REALLY cool page!!!  Let's play some games shall we?");
-                    alert("To view this page you must first guess my age.");
+    alert("Welcome to this REALLY cool page!!!  Let's play some games shall we?");
+    alert("To view this page you must first guess my age.");
 
-  while (myAge) {
-                    var guessAge = prompt ("How old am I?");
-                    console.log("User has entered the number" + " " + guessAge);
+// // Age guess while loop //
+  var myAge = 28
+  var ageCounter = 0
+while (myAge) {
+    var guessAge = parseInt(prompt ("How old am I?"));
+    console.log("User has entered the number" + " " + guessAge);
 
-    if (guessAge == myAge[0] || guessAge.toLowerCase() == myAge[1] || guessAge.toLowerCase() == myAge[2] || guessAge.toLowerCase() == myAge[3] ) {
-                    correctGuess += 1;
-                    alert("Alright hot shot, good job.\nNow I'm thinking of a number between 1 and 10.  Can you guess it?");
-                    console.log("User guessed correct");
-                    break;
+if (guessAge == 28 ) {
+    correctGuess += 1;
+    ageCounter += 1;
+    alert("Alright hot shot, good job.  That only took you " + ageCounter + " guesses!\nNow I'm thinking of a number between 1 and 10.  Can you guess it?");
+    console.log("User guessed correct");
+    break;
 
-          } else if (guessAge > 32) {
-                    alert("Your guess is way too high!");
-                    console.log("User guessed way too high");
+} else if (guessAge > 32) {
+    alert("Your guess is way too high!  I'm not the Crypt Keeper...");
+    ageCounter += 1;
+    console.log("User guessed way too high");
 
-          } else if (guessAge >= 29 && guessAge <= 32) {
-                    alert("Your guess is too high but very close.  Try a lower number!");
-                    console.log("User guessed too high");
+} else if (guessAge >= 29 && guessAge <= 32) {
+    alert("Your guess is too high but very close.  Try a lower number!");
+    ageCounter += 1;
+    console.log("User guessed too high");
 
-          } else if (guessAge < 23) {
-                    alert("Your guess is way too low!");
-                    console.log("User guessed way too low");
+} else if (guessAge < 23) {
+    alert("Your guess is way too low!  Although I have been using a new face cream, so thanks for noticing...");
+    ageCounter += 1;
+    console.log("User guessed way too low");
 
-          } else if (guessAge >= 23 && guessAge <= 27) {
-                    alert("Your guess is too low but very close.  Try a higher number!");
-                    console.log("User guessed too low");
+} else if (guessAge >= 23 && guessAge <= 27) {
+    alert("Your guess is too low but very close.  Try a higher number!");
+    ageCounter += 1;
+    console.log("User guessed too low");
 
-          } else if (guessAge = isNaN) {
-                    alert("That answer is downright wrong.  Try guessing with an integer!");
-                    console.log("User guessed with a string"); } }
+} else if (guessAge = isNaN) {
+    alert("That answer is downright wrong.  Try guessing with an integer!");
+    ageCounter += 1;
+    console.log("User guessed with a string");
+  }
+}
+
+// // My number while loop //
+  var chances = 3
+  var myNumber = 8
+
+while (myNumber && chances > 0){
+    var guessNumber =prompt("What is my number?  You have " + chances + " guesses remaining.");
+
+if (guessNumber == 8 ) {
+    alert("Right you are!  8 is the greatest number in all of the land!");
+    console.log("User correctly guessed the number 8");
+    correctGuess += 1;
+    break;
+
+} else if ( chances == 1) {
+    alert("Bummer!  Looks like you're out of chances...Better luck next time.");
+    chances -=1;
+
+} else if (guessNumber < 8 || guessNumber > 8) {
+    alert("Oh dear...That's not right at all.  Try again!");
+    console.log("User guessed wrong");
+    chances -= 1;
+
+} else if (guessNumber = isNaN) {
+    alert("That answer is downright wrong.  Try guessing with an integer!");
+    chances -=1;
+    console.log("Use guessed with a string");
+  }
+}
+
+  var answerArr = []
+// Function practice //
+
+  function monthPythonGame(question){
+  var answer = prompt(question).toLowerCase();
+
+while (answer != "" || answer != 0) {
+  if (answer == "") {
+    alert("What are you doing!?  You have to enter SOMETHING!");
+
+  } else if (answer >= 0 || answer <= 0) {
+    alert("The number games are over - Use your words!");
+    return prompt(question)
+
+  } else {
+    correctGuess +=1;
+  }
+  answerArr.unshift(answer);
+  console.log("User has entered " + answerArr[0])
+  break;
+  }
+}
+
+monthPythonGame("What...is your name?")
+monthPythonGame("What...is your quest?")
 
 
-  while (myNumber){
-                    var guessNumber = prompt("What is my number?");
+  var favColor = prompt("What...Is your favorite color?");
+      console.log(answerArr[1] + "'s favorite color is " + favColor.toLowerCase());
 
-    if (guessNumber != 8 ) {
-                    alert("Wrong!  Try again.");
-                    console.log("User guessed incorrectly");
-                    myNumberCount += 1
+if (favColor.toLowerCase() === "purple") {
+    correctGuess += 1
+    document.title = (answerArr[0].toUpperCase()+("!!!"));
+    alert("Excellent work " + answerArr[1] + ", you have chosen wisely!\nYou have guessed " + correctGuess + " out of 5 questions right!\nNow enjoy this pleasantly purple page!");
+    console.log(answerArr[1] + " has amazing taste and will now be shown this awesome page");
+    document.getElementById('hiddenContent').style.display= "block";
+    document.getElementById("name").innerHTML = ("CONGRATULATIONS " + answerArr[1].toUpperCase() + "!!!");
 
-          } else {
-                    correctGuess += 1
-                    myNumberCount += 1
-                    alert("Impressive!  That only took you" + " " + myNumberCount + " " + "tries.\nBut to see this page you must first answer these three questions...");
-                    console.log("User guessed the number 8 after" + " " + myNumberCount + " " + "tries.");
-                    break; } }
-
-                    function spellName(){
-                    var userName = prompt("What...Is your name?");
-                    console.log("User's name is" + " " + userName);
-
-    if (userName === "" ) {
-                    alert("What are you doing!?  You have to enter SOMETHING!");
-                    console.log("User failed to enter anything");
-                    spellName();
-
-          } else if (userName >= 0 || userName <= 0) {
-                    alert("I highly doubt your name is a number... Spell out your name!");
-                    console.log("User entered an integer as their name");
-                    spellName();
-
-          } else {
-                    correctGuess += 1
-                    var quest = prompt("What...Is your quest?");
-                    console.log(userName + "'s ultimate goal is" + " " + quest.toLowerCase()); }
-
-    if (quest.toLowerCase() === "holy grail" || quest.toLowerCase() === "to find the holy grail" || quest.toLowerCase() === "to seek the holy grail" || quest.toLowerCase() === "to obtain the holy grail") {
-                    correctGuess += 1
-                    alert("You sure do know your Monty Python references!  You got the bonus point!");
-                    function monthPython(){
-                    var favColor = prompt("What...Is your favorite color?");
-                    console.log(userName + "'s favorite color is" + " " + favColor.toLowerCase());
-
-    if (favColor.toLowerCase() === "purple") {
-                    correctGuess += 1
-                    document.title = (quest.toUpperCase()+("!!!"));
-                    alert("Excellent work" + " " + userName + ", " + "you have chosen wisely!\nYou have guessed" + " " + correctGuess + " " + "out of 5 questions right!\nNow enjoy this pleasantly purple page!");
-                    console.log(userName + " " + "has amazing taste and will now be shown this awesome page");
-                    document.getElementById('hiddenContent').style.display= "block";
-                    document.getElementById("name").innerHTML = ("CONGRATULATIONS" + " " + userName.toUpperCase() + "!!!");
-
-          } else {
-                    console.log(userName + " " + "has poor taste and will now be ejected from the website");
-                    alert("Okay" + " " + userName + ", " + "that's nice, but" + " " + favColor.toLowerCase() + " " + "is a terrible color so BYE FELICIA!");
-                    window.close(); }
-                    }monthPython();
-
-          } else {
-                    monthPython();
-          }
-                    }spellName();
+} else {
+    console.log(answerArr[1] + " has poor taste and will now be ejected from the website");
+    alert("Okay " + answerArr[1] + " , that's nice, but " + favColor.toLowerCase() +  " is a terrible color so get off my page!");
+    window.close(); }
